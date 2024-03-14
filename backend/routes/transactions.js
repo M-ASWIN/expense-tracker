7c39aecf93eb561acc7305572cbdff242c37936b
Aswin M <aswinm.cse2021@citchennai.net>
2024-03-14 23:02:59 +0530

    initial commit

---
+++ backend/routes/transactions.js
@@ -0,0 +1,15 @@
+const {addIncome} =require('../controllers/income');
+const {getIncomes} =require('../controllers/income');
+const {deleteIncome} =require('../controllers/income');
+const {addExpense} =require('../controllers/expense');
+const {getExpense} =require('../controllers/expense');
+const {deleteExpense} =require('../controllers/expense');
+const router=require('express').Router()
+
+router.post('/add-income', addIncome)
+        .get('/get-income', getIncomes)
+        .delete('/delete-income/:id', deleteIncome)
+        .post('/add-expense', addExpense)
+        .get('/get-expense', getExpense)
+        .delete('/delete-expense/:id', deleteExpense)
+module.exports=router
